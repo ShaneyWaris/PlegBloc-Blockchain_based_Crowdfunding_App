de84fc9545cc1926aefa8ff35dee38c35a02d31e
Shaney Waris <shaney18308@iiitd.ac.in>
2022-01-08 18:19:13 +0530

    request card UI added

--- frontend/src/main_page/Request_Card.js
+++ frontend/src/main_page/Request_Card.js
@@ -2,9 +2,32 @@ import React from "react";
 
 function Request_Card() {
   return (
-    <div>
-      <h1></h1>
-    </div>
+    <>
+      <div className="our_solution_category">
+                  <div className="solution_cards_box">
+                      <div className="solution_card" style={{background:"#2A3544"}}>
+                          <div className="solu_title text-center">
+                            <h6 style={{color:"whitesmoke"}}>Request #1</h6>
+                          </div>
+                          <div className="solu_title">
+                            <h5 style={{color:"whitesmoke"}}><b><u>Hiring Advertisers</u></b></h5>
+                          </div>
+                          <div className="solu_description" style={{color:"whitesmoke", textAlign: "justify"}}>
+                            <p className="mb-2"><b>Description</b> : request description request description request description request description request description request description</p>
+                            <p className="mb-2"><b>Number of Backers</b> : 1</p>
+                            <p className="mb-2"><b>Number of Votes</b> : 0</p>
+                            <p className="mb-2"><b>Amount Requested</b> : 1 Eth</p>
+                            <p className="mb-3"><b>Vendor</b> : Abhinav vendor &nbsp; <a style={{float:"right"}}><u>Vendor Details</u> &nbsp; ↗</a></p>
+                            <button type="button" className="read_more_btn"
+                            style={{backgroundColor: "SlateGrey"}}
+                            >
+                              Approve Request
+                            </button>
+                          </div>
+                      </div>
+                  </div>
+        </div>
+    </>
   );
 }
 
--- frontend/src/main_page/RequestsPage.js
+++ frontend/src/main_page/RequestsPage.js
@@ -1,9 +1,25 @@
 import React from "react";
+import Request_Card from "./Request_Card";
 
 function RequestsPage() {
   return (
     <>
-      <h1></h1>
+      <div className="my-5">
+        <h1 className="text-center">All Requests</h1>
+      </div>
+      <div className="container-fluid mb-5">
+        <div className="row">
+          <div className="col-10 mx-auto">
+            <div className="row gy-4">
+              <Request_Card />
+              <Request_Card />
+              <Request_Card />
+              <Request_Card />
+              <Request_Card />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
